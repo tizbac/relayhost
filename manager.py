ab703ad7964df3f97921ef40867a3aef4eaf419e
tiziano <tiziano@tiziano-desktop-amd64.(none)>
2009-04-25 15:59:56 +0200

    * Added Ban System to Manager * Fixed a possible exploit

--- manager.py
+++ manager.py
@@ -18,9 +18,10 @@ class Main:
 	disabled = False
 	def botthread(self,nick,s,r,p,ist):
 		try:
-			logc(s,"Spawning "+nick)
+			logc(s,"Spawning (Requested by %s) " % r +nick)
 			d = dict()
 			d.update([("serveraddr",self.app.config["serveraddr"])])
+			d.update([("spawnedby",r)])
 			d.update([("serverport",self.app.config["serverport"])])
 			d.update([("springdedpath",self.app.config["springdedpath"])])
 			d.update([("admins",self.app.config["admins"])])
@@ -45,10 +46,13 @@ class Main:
 			print '-'*60
 	def onload(self,tasc):
 		self.tsc = tasc
+		self.bans = []
+		self.config = readconfigfile("Manager.conf")
+		self.bans = parselist(self.config["bans"],",")
 		self.app = tasc.main
 		self.an = parselist(self.app.config["accountsnick"],",")
 		self.ap = parselist(self.app.config["accountspass"],",")
-		self.disabled = bool(int(self.app.config["enabled"]))
+		self.disabled = not bool(int(self.app.config["enabled"]))
 	def oncommandfromserver(self,command,args,socket):
 		try:
 			if command == "SAIDPRIVATE" and len(args) == 2 and args[1] == "!enable" and args[0] in self.app.admins:
@@ -57,13 +61,49 @@ class Main:
 				socket.send("SAYPRIVATE %s %s\n" % (args[0],"Hosting new games enabled"))
 				self.app.config["enabled"] = "1"
 				self.app.SaveConfig()
-			if command == "SAIDPRIVATE" and len(args) == 2 and args[1] == "!disable" and args[0] in self.app.admins:
+			elif command == "SAIDPRIVATE" and len(args) == 2 and args[1] == "!disable" and args[0] in self.app.admins:
 				self.disabled = True
 				socket.send("MYSTATUS %i\n" % int(int(self.listfull)+int(self.disabled)*2))
 				socket.send("SAYPRIVATE %s %s\n" % (args[0],"Hosting new games disabled"))
 				self.app.config["enabled"] = "0"
 				self.app.SaveConfig()
-			if command == "SAIDPRIVATE" and len(args) == 2 and args[1] == "!spawn" and args[0] not in self.ul and len(self.ul) < len(self.an) and not self.disabled:
+			elif command == "SAIDPRIVATE" and len(args) == 2 and args[1] == "!listbans" and args[0] in self.app.admins:
+				x = 0
+				l0 = []
+				for b in self.bans:
+					l0.append(b)
+					x += 1
+					if x >= 5:
+						socket.send("SAYPRIVATE %s %s\n" % (args[0],' | '.join(l0)))
+						x = 0
+						l0 = []
+				socket.send("SAYPRIVATE %s %s\n" % (args[0],' | '.join(l0)))
+			elif command == "SAIDPRIVATE" and len(args) >= 3 and args[1] == "!ban" and args[0] in self.app.admins:
+				toban = args[2:]
+				for b in toban:
+					if not b in self.bans:
+						self.bans.append(b)
+						socket.send("SAYPRIVATE %s %s\n" % (args[0],b+" Banned"))
+					else:
+						socket.send("SAYPRIVATE %s %s\n" % (args[0],b+" is already banned"))
+				self.config["bans"] = ','.join(self.bans)
+				writeconfigfile("Manager.conf",self.config)
+				socket.send("SAYPRIVATE %s %s\n" % (args[0],"Done."))
+			elif command == "SAIDPRIVATE" and len(args) >= 3 and args[1] == "!unban" and args[0] in self.app.admins:
+				toban = args[2:]
+				for b in toban:
+					if not b in self.bans:
+						socket.send("SAYPRIVATE %s %s\n" % (args[0],b+"is not currently banned"))
+					else:
+						self.bans.remove(b)
+						socket.send("SAYPRIVATE %s %s\n" % (args[0],b+" has been unbanned"))
+				self.config["bans"] = ','.join(self.bans)
+				writeconfigfile("Manager.conf",self.config)
+				socket.send("SAYPRIVATE %s %s\n" % (args[0],"Done."))
+			elif command == "SAIDPRIVATE" and len(args) == 2 and args[1] == "!spawn" and args[0] not in self.ul and len(self.ul) < len(self.an) and not self.disabled:
+				if args[0] in self.bans:
+					socket.send("SAYPRIVATE %s %s\n" %(args[0],"You are banned!"))
+					return
 				self.threads.append(thread.start_new_thread(self.botthread,(self.an[len(self.ul)],socket,args[0],self.ap[len(self.ul)],self)))
 				socket.send("SAYPRIVATE %s %s\n" %(args[0],self.an[len(self.ul)]))
 				self.ul.append(args[0])
